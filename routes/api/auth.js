502ec294301a6a22fa413990d03c726db2bcf79f
Charles-Hebert Dalzon <charlby5@gmail.com>
2020-07-20 17:09:36 -0400

    Create a custom middleware, verify the token and get the auth's info

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,21 @@
 const express = require('express');
 
+const auth = require('../../middleware/auth');
+const User = require('../../models/User');
+
 const router = express.Router();
 
 // @route   GET /api/auth
 // @desc    Test Route
 // @access  Public
-router.get('/', (req, res) => res.send('Auth Route'));
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send('Server error');
+  }
+});
 
 module.exports = router;
