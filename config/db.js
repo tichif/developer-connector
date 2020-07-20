a3e361ca05c07031607f9606e1b70a3ca7c4a8ca
Charles-Hebert Dalzon <charlby5@gmail.com>
2020-07-20 14:09:21 -0400

    Connect the add to MongoDB Atlas and create routes

---
+++ config/db.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const config = require('config');
+
+// get the MongoDB connection in the default.json
+const db = config.get('mongoURI');
+
+// Connect to the database
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected...');
+  } catch (err) {
+    console.log(err.message);
+    // exit the process with failure
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
