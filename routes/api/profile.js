d9cc45ceecf0aa462a756ffc66d022adbdef8064
Charles-Hebert Dalzon <charlby5@gmail.com>
2020-07-21 14:22:45 -0400

    Delete user, profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -158,4 +158,24 @@ router.get('/', async (req, res) => {
   }
 });
 
+// @route   DELETE /api/profile
+// @desc    Delete profile, user, posts
+// @access  Private
+router.delete('/', auth, async (req, res) => {
+  try {
+    // @todo - remove user's posts
+
+    // remove profile
+    await Profile.findOneAndRemove({ user: req.user.id });
+
+    // remove user
+    await User.findOneAndRemove({ _id: req.user.id });
+
+    res.json({ msg: 'User removed' });
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
 module.exports = router;
