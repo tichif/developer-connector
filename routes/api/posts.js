e4900782e09a5ff9ffa8e967f0441e987670cd26
Charles-Hebert Dalzon <charlby5@gmail.com>
2020-07-22 21:40:31 -0400

    Create Post Model and add Post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,10 +1,53 @@
 const express = require('express');
+const { body, validationResult } = require('express-validator');
+
+const auth = require('../../middleware/auth');
+const Post = require('../../models/Post');
+const User = require('../../models/User');
+const Profile = require('../../models/Profile');
 
 const router = express.Router();
 
-// @route   GET /api/posts
-// @desc    Test Route
-// @access  Public
-router.get('/', (req, res) => res.send('Posts Route'));
+// @route   POST /api/posts
+// @desc    Create a Post
+// @access  Private
+router.post(
+  '/',
+  [auth, [body('text', 'The content is required').notEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+
+      if (!user) {
+        return res.status(401).json({
+          errors: [
+            {
+              msg: 'User not found',
+            },
+          ],
+        });
+      }
+
+      const post = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      });
+
+      const result = await post.save();
+
+      res.send(result);
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 module.exports = router;
