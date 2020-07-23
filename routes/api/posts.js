ea5f31217b0b4d4cd4ec44bc6d54620617e83d3a
Charles-Hebert Dalzon <charlby5@gmail.com>
2020-07-23 09:45:09 -0400

    Get and delete a post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -50,4 +50,95 @@ router.post(
   }
 );
 
+// @route   GET /api/posts
+// @desc    Get all posts
+// @access  Private
+router.get('/', auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+    res.json(posts);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route   GET /api/posts/:post_id
+// @desc    Get single post by ID
+// @access  Private
+router.get('/:post_id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.post_id);
+
+    if (!post) {
+      return res.status(404).json({
+        errors: [
+          {
+            msg: 'Post not found',
+          },
+        ],
+      });
+    }
+    res.json(post);
+  } catch (err) {
+    console.log(err.message);
+    if (err.kind === 'ObjectId') {
+      return res.status(404).json({
+        errors: [
+          {
+            msg: 'Post not found',
+          },
+        ],
+      });
+    }
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route   DELETE /api/posts/:post_id
+// @desc    Remove a post by ID
+// @access  Private
+router.delete('/:post_id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.post_id);
+
+    if (!post) {
+      return res.status(404).json({
+        errors: [
+          {
+            msg: 'Post not found',
+          },
+        ],
+      });
+    }
+
+    // check on the user
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({
+        errors: [
+          {
+            msg: 'Unauthorized action.',
+          },
+        ],
+      });
+    }
+
+    await post.remove();
+
+    res.json({ msg: 'Post removed' });
+  } catch (err) {
+    console.log(err.message);
+    if (err.kind === 'ObjectId') {
+      return res.status(404).json({
+        errors: [
+          {
+            msg: 'Post not found',
+          },
+        ],
+      });
+    }
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
